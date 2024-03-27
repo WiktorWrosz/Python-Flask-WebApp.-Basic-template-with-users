997773b28bd57af278502aeec74581abadb1fa90
Wiktor <wiktorwrosz@o2.pl>
2024-03-27 23:43:48 +0100

    added 3 auth routes

--- website/auth.py
+++ website/auth.py
@@ -3,3 +3,15 @@ from flask import Blueprint
          # routes define the mapping between URLs and the functions that handle requests to those URLs. 
 
 auth = Blueprint("auth",__name__) # Define name of this blueprint == auth, same as file name for simplicity
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign up</p>"
